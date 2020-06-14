fed773423320601434fbf1bffadf1fb449f51f18
Ayrat <ayrat1@mail.ru>
2020-06-14 16:09:23 +0300

    100

---
+++ src/main/java/de/tacocloud/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package de.tacocloud.repository;
+
+import de.tacocloud.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
