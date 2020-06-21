4f12e7343185e8d8d4a5fac32d5dd0d7cedf74be
Ayrat <ayrat1@mail.ru>
2020-06-21 15:13:21 +0300

    170

--- src/main/java/de/tacocloud/domain/User.java
+++ src/main/java/de/tacocloud/domain/User.java
@@ -21,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 public class User implements UserDetails {
 
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
